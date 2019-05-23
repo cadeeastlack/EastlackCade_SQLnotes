541cd32d617d20c71378b79c57d383c4f789f2fc
cadeeastlack <47193589+cadeeastlack@users.noreply.github.com>
2019-05-23 11:22:25 -0700

    Update

---
+++ app/src/main/java/com/example/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,37 @@
+package com.example.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String  DATABASE_NAME = "Contact2019.db";
+    public static final String  TABLE_NAME = "Contact2019_table";
+    public static final String  ID = "ID";
+    public static final String  COLUMN_NAME_CONTACT = "contact";
+
+    public static final String  SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NAME_CONTACT + " TEXT)";
+
+
+    public DatabaseHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory: null, DATABASE_VERSION);
+        SQLiteDatabase db = this.getWritableDatabase(); //for test only - will remove later
+        Log.d(tag: "MyContactApp", msg: "DatabaseHelper: constructed DatabaseHelper");
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
